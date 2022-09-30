d8bf7e9160468aa0526c90e987958e3a2daee1c4
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-30 16:50:55 -0500

    useEffect in moviedetails implemented

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,8 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export default function MovieDetails() {
 
+    const dispatch = useDispatch();
     const params = useParams();
+
+    useEffect(() => {
+
+        dispatch({
+            type: 'SAGA_FETCH_DETAILS',
+            payload: params.id
+        });
+
+        return () => {
+            dispatch({type: 'CLEAR_DETAILS'});
+        }
+
+    }, [params.id])
 
     return (
         <>
