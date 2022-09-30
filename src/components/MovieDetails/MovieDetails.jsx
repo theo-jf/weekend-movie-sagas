735ff46ff26009f2509f039a39abacee7cffc405
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-30 17:03:30 -0500

    Basic movie details render setup

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function MovieDetails() {
 
+    // FIX RENDERING BUG!!!!!
+
     const dispatch = useDispatch();
     const params = useParams();
+    const movie = useSelector(store => store.details);
+    const genres = movie.genres
 
     useEffect(() => {
 
@@ -21,7 +25,18 @@ export default function MovieDetails() {
     }, [params.id])
 
     return (
-        <>
-        </>
+        <div key={movie.id} >
+        <h3>{movie.title}</h3>
+        <img className="detailsPoster" 
+            src={movie.poster} 
+            alt={movie.title}
+        />
+        <p>{(genres.length > 1) ? 'Genres' : 'Genre'}</p>
+        {movie.genres.map(genre => {
+            return (
+                <p>{genre}</p>
+            );
+        })}
+        </div>
     );
 }
